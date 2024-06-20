a789194ee7defe4738a15b275dd612126e3ebe46
Mark Dave Lorejo <dave.lorejo69@gmail.com>
2024-06-20 13:28:18 +0800

    added text

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import OpulentLogo from './assets/LOGO-SOLO-Opulent.png'
-import './App.css'
+import { useState } from "react";
+import OpulentLogo from "./assets/LOGO-SOLO-Opulent.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -12,14 +12,18 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a> */}
 
-          <img src={OpulentLogo} className="logo" alt="logo" />
-
+        <img src={OpulentLogo} className="logo" alt="logo" />
       </div>
       <h1>Opulent Adobes</h1>
       <h2>Coming Soon!</h2>
-      
+      <div style={{ color: "white", marginTop: "20%" }}>
+        <h4>Site Tour Scheduler is under development.</h4>
+        <a href="https://www.facebook.com/profile.php?id=61558432993334" target="_blank">
+          <h4>Please send a direct message to our Facebook page for inquiries.</h4>
+        </a>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
